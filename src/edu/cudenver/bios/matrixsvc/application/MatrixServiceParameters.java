0f0a48f5a9b687b84508559bc91b760d6f59f735
Jonathan Cohen <jonathan.cohen@ucdenver.edu>
2011-04-19 16:26:07 +0000

    rewrite, there are now 2 lists of RealMatrix objects, and the other possible parts of any request/response xml.

--- src/edu/cudenver/bios/matrixsvc/application/MatrixServiceParameters.java
+++ src/edu/cudenver/bios/matrixsvc/application/MatrixServiceParameters.java
@@ -2,107 +2,60 @@ package edu.cudenver.bios.matrixsvc.application;
 
 import org.apache.commons.math.linear.RealMatrix;
 
-public class MatrixServiceParameters {
-	private RealMatrix sum = null;
-	private RealMatrix difference = null;
-	private RealMatrix product = null;
-	private RealMatrix A = null;
-	private RealMatrix B = null;
-	private RealMatrix inverse = null;
-	private RealMatrix positiveDefinite = null;
-	private RealMatrix L = null;
-	private RealMatrix lTranspose = null;
-	private RealMatrix vec = null;
-	private RealMatrix vech = null;
+import java.util.ArrayList;
 
-	private int scalarMultiplier = -1;
-	private int trace = -1;
-	private int rank = -1;
+public class MatrixServiceParameters {
 	
-	public RealMatrix getProduct() {
-		return product;
+	private ArrayList<RealMatrix> matrixListFromRequest = null;
+	private ArrayList<RealMatrix> matrixListForResponse = null;
+	private Double scalarMultiplier = null;
+	private Double trace = null;
+	private Double rank = null;
+	
+	public void addMatrixToRequestList(RealMatrix matrix){
+		if(matrix != null){
+			matrixListFromRequest.add(matrix);
+		}
 	}
-	public void setProduct(RealMatrix product) {
-		this.product = product;
+	
+	public void addMatrixToResponseList(RealMatrix matrix){
+		if(matrix != null){
+			matrixListForResponse.add(matrix);
+		}
 	}
-	public RealMatrix getSum() {
-		return sum;
+	
+	public ArrayList<RealMatrix> getMatrixListFromRequest() {
+		return matrixListFromRequest;
 	}
-	public void setSum(RealMatrix sum) {
-		this.sum = sum;
+	public void setMatrixListFromRequest(ArrayList<RealMatrix> matrixListFromRequest) {
+		this.matrixListFromRequest = matrixListFromRequest;
 	}
-	public RealMatrix getDifference() {
-		return difference;
+	public ArrayList<RealMatrix> getMatrixListForResponse() {
+		return matrixListForResponse;
 	}
-	public void setDifference(RealMatrix difference) {
-		this.difference = difference;
+	public void setMatrixListForResponse(ArrayList<RealMatrix> matrixListForResponse) {
+		this.matrixListForResponse = matrixListForResponse;
 	}
-	public RealMatrix getA() {
-		return A;
-	}
-	public void setA(RealMatrix a) {
-		A = a;
-	}
-	public RealMatrix getB() {
-		return B;
-	}
-	public void setB(RealMatrix b) {
-		B = b;
-	}
-	public RealMatrix getInverse() {
-		return inverse;
-	}
-	public void setInverse(RealMatrix inverse) {
-		this.inverse = inverse;
-	}
-	public RealMatrix getPositiveDefinite() {
-		return positiveDefinite;
-	}
-	public void setPositiveDefinite(RealMatrix positiveDefinite) {
-		this.positiveDefinite = positiveDefinite;
-	}
-	public RealMatrix getL() {
-		return L;
-	}
-	public void setL(RealMatrix l) {
-		L = l;
-	}
-	public RealMatrix getlTranspose() {
-		return lTranspose;
-	}
-	public void setlTranspose(RealMatrix lTranspose) {
-		this.lTranspose = lTranspose;
-	}
-	public RealMatrix getVec() {
-		return vec;
-	}
-	public void setVec(RealMatrix vec) {
-		this.vec = vec;
-	}
-	public RealMatrix getVech() {
-		return vech;
-	}
-	public void setVech(RealMatrix vech) {
-		this.vech = vech;
-	}
-	public int getScalarMultiplier() {
+	public double getScalarMultiplier() {
 		return scalarMultiplier;
 	}
-	public void setScalarMultiplier(int scalarMultiplier) {
+	public void setScalarMultiplier(double scalarMultiplier) {
 		this.scalarMultiplier = scalarMultiplier;
 	}
-	public int getTrace() {
+	public double getTrace() {
 		return trace;
 	}
-	public void setTrace(int trace) {
+	public void setTrace(double trace) {
 		this.trace = trace;
 	}
-	public int getRank() {
+	public double getRank() {
 		return rank;
 	}
-	public void setRank(int rank) {
+	public void setRank(double rank) {
 		this.rank = rank;
 	}
 	
-	 
+	
+	
 }
+
